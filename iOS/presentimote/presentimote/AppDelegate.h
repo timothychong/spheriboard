06e459976af99cc592c6e615e15edeeacbff858f
Timothy Chong <timothychong@outlook.com>
2014-09-13 02:13:39 -0400

    Initial project commit

---
+++ iOS/presentimote/presentimote/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  presentimote
+//
+//  Created by Timothy Chong on 9/13/14.
+//  Copyright (c) 2014 Downtyne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS/presentimote/presentimote/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  presentimote
+//
+//  Created by Timothy Chong on 9/13/14.
+//  Copyright (c) 2014 Downtyne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
