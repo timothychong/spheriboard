89ae609bf6986aa6088e56ec196e7dbfccc28ea4
John Moore <jmoore15@bu.edu>
2014-09-13 09:14:18 -0400

    Adding initial server code

--- server/server.js
+++ server/server.js
@@ -1,10 +1,54 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+mongoose.connect('mongodb://localhost/spheriboard');
 
-var Cat = mongoose.model('Cat', { name: String });
-
-var kitty = new Cat({ name: 'Zildjian' });
-kitty.save(function (err) {
-  if (err) // ...
-  console.log('meow');
+var Coord = new Schema({
+	x  : Number,
+		y  : Number
 });
+
+var Segment = new Schema({
+	s: Coord,
+	e: Coord
+});
+
+var Drawing = mongoose.model('drawing', new Schema({
+	channel  :  String,
+	owner  :  String,
+	segs  :  [Segment]
+}));
+
+var io = require('socket.io').listen(42069, {log: true});
+io.sockets.on('connection', function (socket) {
+	socket.on('subscribe', function(data){
+		socket.join(data.channel);
+		socket['uid'] = data.uid;
+	});
+
+	socket.on('savedrawing', function(data){
+		//socket.broadcast.to(data.channel).emit(data);
+
+		var drawing = new Drawing({'channel': data.channel, 'owner': socket.uid, 'segs': data.segs});
+
+		drawing.save(function (err) {
+		  if (err) {
+		  	console.log('error saving drawing: ' + err);
+		  }
+		  console.log('saved drawing');
+		});
+	});
+	 
+	socket.on('disconnect', function(topic){
+		socket.leave(topic);
+	});
+
+	socket.on('getdrawings', function(data){
+		Drawing.find({ channel: data.channel }).exec(function(err, drawings) {
+  			if (err) return console.log(err);
+  			console.log(drawings);
+		});
+	});
+});
+
+
+
+
